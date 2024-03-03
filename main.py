fde069072f4ab5acc3e5d5ea1f0f92d5906e8099
manas <bagrimanasbir@gmail.com>
2024-03-02 21:42:12 -0800

    added more data!!

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 
 # enable some matplotlib patches for plotting timedelta values and load
 # FastF1's default color scheme
-# fastf1.Cache.clear_cache()
+#fastf1.Cache.clear_cache()
 fastf1.plotting.setup_mpl(misc_mpl_mods=False)
 max_engine_power = 661949
 fig, ax = plt.subplots()
@@ -27,7 +27,7 @@ teams = {
     'McLaren': ['PIA', 'NOR'],
     'Mercedes': ['HAM', 'RUS'],
     'Red Bull': ['VER', 'PER'],
-    'williams': ['ALB', 'SAR']}
+    'Williams': ['ALB', 'SAR']}
 
 races = [
     "Bahrain Grand Prix",
@@ -54,13 +54,36 @@ races = [
     "Abu Dhabi Grand Prix"
 ]
 
-data = {}
-drs_data = {}
+data = {
+    'Alfa Romeo': {},
+    'Alpha Tauri': {},
+    'Alpine': {},
+    'Aston Martin': {},
+    'Ferrari': {},
+    'Haas': {},
+    'McLaren': {},
+    'Mercedes': {},
+    'Red Bull': {},
+    'Williams': {}
+}
+drs_data = {
+    'Alfa Romeo': {},
+    'Alpha Tauri': {},
+    'Alpine': {},
+    'Aston Martin': {},
+    'Ferrari': {},
+    'Haas': {},
+    'McLaren': {},
+    'Mercedes': {},
+    'Red Bull': {},
+    'Williams': {}
+}
 
 for race in races:
     session = fastf1.get_session(2023, race, 'qualifying')
     session.load()
     for team in teams:
+
         for driver in teams[team]:
             laps = session.laps.pick_driver(driver)
             try:
@@ -85,23 +108,25 @@ for race in races:
 
             
             for index, row in lap.iterrows():
-                if row['Acceleration'] < 0 or row['RPM'] < 11000 or row['Throttle'] != 100 or row['nGear'] != 8:
+                if row['Acceleration'] < 0 or row['RPM'] < 11000 or row['Throttle'] != 100:
                     lap.drop(index, inplace=True)
                 else:
                     lap.loc[index, 'v^2 (m^2/s^4)'] = (row['Speed'] ** 2)
                     lap.loc[index, 'F_lost (N)'] = (((row['RPM']/rpm_max) * max_engine_power)/row['Speed']) - (f1_car_weight * row['Acceleration'])
 
-            if team not in data:
+            if race not in data[team]:
                 data[team][race] = lap
                 drs_data[team][race] = drs_lap
-                if 'all' not in data[team].values():
-                    data[team]['all'] = lap
-                    drs_data[team]['all'] = drs_lap
             else:
                 data[team][race] = pd.concat([data[team][race], lap], ignore_index=True)
-                drs_data[team][race] = pd.concat([drs_data[team][race], lap], ignore_index=True)
-                data[team]['all'] = pd.concat([data[team][race], lap], ignore_index=True)
-                drs_data[team]['all'] = pd.concat([drs_data[team][race], lap], ignore_index=True)
+                drs_data[team][race] = pd.concat([drs_data[team][race], drs_lap], ignore_index=True)
+
+            if 'all' not in data[team]:
+                data[team]['all'] = lap
+                drs_data[team]['all'] = drs_lap
+            else:
+                data[team]['all'] = pd.concat([data[team]['all'], lap], ignore_index=True)
+                drs_data[team]['all'] = pd.concat([drs_data[team]['all'], drs_lap], ignore_index=True)
 
 output_directory = "team_data"
 if not os.path.exists(output_directory):
@@ -120,6 +145,100 @@ for team in drs_data:
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
     drs_data[team]['all'].to_csv(os.path.join(output_folder, "drs_data.csv"), index=False)
+
+
+
+for team in data:
+    #plt.scatter(data[team]['all']['v^2 (m^2/s^4)'], data[team]['all']['F_lost (N)'], label=team)
+    regressions_data = []
+    for race in data[team]:
+        # Perform linear regression
+    
+        
+        try:
+            coeffs = np.polyfit(data[team][race]['v^2 (m^2/s^4)'], data[team][race]['F_lost (N)'], 1)
+        except:
+            print("skipped " + race + ' for '+ team + ' no DRS')
+            continue
+
+        regression_line = np.poly1d(coeffs)
+
+        # Plot regression line
+        #plt.plot(data[team][race]['v^2 (m^2/s^4)'], regression_line(data[team][race]['v^2 (m^2/s^4)']), label=f'Regression Line ({team})')
+        # Display equation of regression line
+        #plt.text(0.1, 0.9, f"y = {coeffs[0]:.2f}x + {coeffs[1]:.2f}", transform=ax.transAxes)
+        
+        # Calculate coefficients
+        c_d_mul_a = coeffs[0] / avg_air_density_div_2
+        c_rr = coeffs[1] / (normal_force * 4)
+
+
+        # Append data to list
+        regressions_data.append({'Race': race, 'Regression_Line': regression_line, 'C_d_mul_A': c_d_mul_a, 'C_rr': c_rr})
+        regressions_df = pd.DataFrame(regressions_data)
+        regressions_df.to_csv('team_data/' + team +'/regressions_data.csv', index=False)
+
+for team in drs_data:
+    #plt.scatter(data[team]['all']['v^2 (m^2/s^4)'], data[team]['all']['F_lost (N)'], label=team)
+    drs_regressions_data = []
+    for race in drs_data[team]:
+        # Perform linear regression
+    
+        try:
+            drs_coeffs = np.polyfit(drs_data[team][race]['v^2 (m^2/s^4)'], drs_data[team][race]['F_lost (N)'], 1)
+        except:
+            print("skipped " + race + ' for '+ team + ' DRS')
+            continue
+
+        drs_regression_line = np.poly1d(drs_coeffs)
+
+        # Plot regression line
+        #plt.plot(data[team][race]['v^2 (m^2/s^4)'], regression_line(data[team][race]['v^2 (m^2/s^4)']), label=f'Regression Line ({team})')
+        # Display equation of regression line
+        #plt.text(0.1, 0.9, f"y = {coeffs[0]:.2f}x + {coeffs[1]:.2f}", transform=ax.transAxes)
+        
+        # Calculate coefficients
+
+        drs_c_d_mul_a = drs_coeffs[0] / avg_air_density_div_2
+        drs_c_rr = drs_coeffs[1] / (normal_force * 4)
+
+        # Append data to list
+        drs_regressions_data.append({'Race': race, 'Regression_Line': drs_regression_line, 'C_d_mul_A': drs_c_d_mul_a, 'C_rr': drs_c_rr})
+        drs_regressions_df = pd.DataFrame(drs_regressions_data)
+        drs_regressions_df.to_csv('team_data/' + team +'/drs_regressions_data.csv', index=False)
+
+
+# plt.xlabel('v^2 (m^2/s^4)')
+# plt.ylabel('F_lost (N)')
+# plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Team with Regression Lines')
+# plt.legend()
+# plt.show()
+
+# for team in drs_data:
+#     plt.scatter(drs_data[team]['all']['v^2 (m^2/s^4)'], drs_data[team]['all']['F_lost (N)'], label=team)
+
+#     # Perform linear regression
+#     coeffs = np.polyfit(drs_data[team]['all']['v^2 (m^2/s^4)'], drs_data[team]['all']['F_lost (N)'], 1)
+#     regression_line = np.poly1d(coeffs)
+
+#     # Plot regression line
+#     plt.plot(drs_data[team]['all']['v^2 (m^2/s^4)'], regression_line(drs_data[team]['all']['v^2 (m^2/s^4)']), label=f'Regression Line ({team})')
+
+#     # Display equation of regression line
+#     plt.text(0.1, 0.9, f"y = {coeffs[0]:.2f}x + {coeffs[1]:.2f}", transform=ax.transAxes)
+    
+#     # Calculate coefficients
+#     c_d_mul_a = coeffs[0] / avg_air_density_div_2
+#     c_rr = coeffs[1] / (normal_force * 4)
+
+#     # Append data to list
+#     drs_regressions_data.append({'Team': team, 'Regression_Line': regression_line, 'C_d_mul_A': c_d_mul_a, 'C_rr': c_rr})
+
+# plt.xlabel('v^2 (m^2/s^4)')
+# plt.ylabel('F_lost (N)')
+# plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Team with Regression Lines (DRS)')
+# plt.legend()
+# plt.show()
 
 """
 for team, lap_data in data.items():
@@ -142,64 +261,19 @@ plt.legend()
 plt.show() """
 
 
-regressions_data = []
-drs_regressions_data = []
+plt.scatter(data['Red Bull']['Las Vegas Grand Prix']['v^2 (m^2/s^4)'], data['Red Bull']['Las Vegas Grand Prix']['F_lost (N)'], label='RB19')
+plt.xlabel('v^2 (m^2/s^4)')
+plt.ylabel('F_lost (N)')
+plt.title('F_lost (N) vs v^2 (m^2/s^4) for the RB19 without DRS')
+plt.legend()
+plt.show()
 
-for team, lap_data in data.items():
-    plt.scatter(lap_data['v^2 (m^2/s^4)'], lap_data['F_lost (N)'], label=team)
+plt.savefig('LV-RB19-NO-DRS.png', bbox_inches='tight')
 
-    # Perform linear regression
-    coeffs = np.polyfit(lap_data['v^2 (m^2/s^4)'], lap_data['F_lost (N)'], 1)
-    regression_line = np.poly1d(coeffs)
-
-    # Plot regression line
-    plt.plot(lap_data['v^2 (m^2/s^4)'], regression_line(lap_data['v^2 (m^2/s^4)']), label=f'Regression Line ({team})')
-
-    # Display equation of regression line
-    plt.text(0.1, 0.9, f"y = {coeffs[0]:.2f}x + {coeffs[1]:.2f}", transform=ax.transAxes)
-    
-    # Calculate coefficients
-    c_d_mul_a = coeffs[0] / avg_air_density_div_2
-    c_rr = coeffs[1] / (normal_force * 4)
-
-    # Append data to list
-    regressions_data.append({'Team': team, 'Regression_Line': regression_line, 'C_d_mul_A': c_d_mul_a, 'C_rr': c_rr})
-
-# plt.xlabel('v^2 (m^2/s^4)')
-# plt.ylabel('F_lost (N)')
-# plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Team with Regression Lines')
-# plt.legend()
-# plt.show()
-
-for team, drs_lap_data in drs_data.items():
-    plt.scatter(drs_lap_data['v^2 (m^2/s^4)'], drs_lap_data['F_lost (N)'], label=team)
-
-    # Perform linear regression
-    coeffs = np.polyfit(drs_lap_data['v^2 (m^2/s^4)'], drs_lap_data['F_lost (N)'], 1)
-    regression_line = np.poly1d(coeffs)
-
-    # Plot regression line
-    plt.plot(drs_lap_data['v^2 (m^2/s^4)'], regression_line(drs_lap_data['v^2 (m^2/s^4)']), label=f'Regression Line ({team})')
-
-    # Display equation of regression line
-    plt.text(0.1, 0.9, f"y = {coeffs[0]:.2f}x + {coeffs[1]:.2f}", transform=ax.transAxes)
-    
-    # Calculate coefficients
-    c_d_mul_a = coeffs[0] / avg_air_density_div_2
-    c_rr = coeffs[1] / (normal_force * 4)
-
-    # Append data to list
-    drs_regressions_data.append({'Team': team, 'Regression_Line': regression_line, 'C_d_mul_A': c_d_mul_a, 'C_rr': c_rr})
-
-# plt.xlabel('v^2 (m^2/s^4)')
-# plt.ylabel('F_lost (N)')
-# plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Team with Regression Lines (DRS)')
-# plt.legend()
-# plt.show()
-
-# Create DataFrames from lists of dictionaries
-regressions_df = pd.DataFrame(regressions_data)
-drs_regressions_df = pd.DataFrame(drs_regressions_data)
-
-regressions_df.to_csv('regressions_data.csv', index=False)
-drs_regressions_df.to_csv('drs_regressions_data.csv', index=False)
+plt.scatter(drs_data['Red Bull']['Las Vegas Grand Prix']['v^2 (m^2/s^4)'], drs_data['Red Bull']['Las Vegas Grand Prix']['F_lost (N)'], label='RB19')
+plt.xlabel('v^2 (m^2/s^4)')
+plt.ylabel('F_lost (N)')
+plt.title('F_lost (N) vs v^2 (m^2/s^4) for the RB19 with DRS')
+plt.legend()
+plt.show()
+plt.savefig('LV-RB19-DRS.png', bbox_inches='tight')
