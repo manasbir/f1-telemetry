4488a2ec599a33056a452f4d3d770377c27277c2
manas <bagrimanasbir@gmail.com>
2024-03-03 15:43:17 -0800

    pictures !

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ avg_air_density_div_2 = 0.6
 
 teams = {
     'Alfa Romeo': ['BOT', 'ZHO'],
-    'Alpha Tauri': ['RIC', 'DEV', 'TSU', 'LAW'],
+    'AlphaTauri': ['RIC', 'DEV', 'TSU', 'LAW'],
     'Alpine': ['OCO', 'GAS'],
     'Aston Martin': ['ALO', 'STR'],
     'Ferrari': ['LEC', 'SAI'],
@@ -56,7 +56,7 @@ races = [
 
 data = {
     'Alfa Romeo': {},
-    'Alpha Tauri': {},
+    'AlphaTauri': {},
     'Alpine': {},
     'Aston Martin': {},
     'Ferrari': {},
@@ -68,7 +68,7 @@ data = {
 }
 drs_data = {
     'Alfa Romeo': {},
-    'Alpha Tauri': {},
+    'AlphaTauri': {},
     'Alpine': {},
     'Aston Martin': {},
     'Ferrari': {},
@@ -78,6 +78,20 @@ drs_data = {
     'Red Bull': {},
     'Williams': {}
 }
+
+team_to_car = {
+    'Red Bull': 'RB19',
+    'Ferrari': 'SF-23',
+    'Mercedes': 'W14',
+    'Alpine': 'A523',
+    'McLaren': 'MCL60',
+    'Alfa Romeo': 'C43',
+    'Aston Martin': 'AMR23',
+    'Haas': 'VF-23',
+    'AlphaTauri': 'AT04',
+    'Williams': 'FW45'
+}
+
 
 for race in races:
     session = fastf1.get_session(2023, race, 'qualifying')
@@ -91,6 +105,9 @@ for race in races:
             except:
                 continue
             
+            if driver == ['VER'] and race == 'Saudi Arabian Grand Prix':
+                continue
+
             start_time = lap['Date'][0]
             lap['Speed'] /= 3.6
             lap['Time'] = (lap['Date'] - start_time).dt.total_seconds()
@@ -114,6 +131,13 @@ for race in races:
                     lap.loc[index, 'v^2 (m^2/s^4)'] = (row['Speed'] ** 2)
                     lap.loc[index, 'F_lost (N)'] = (((row['RPM']/rpm_max) * max_engine_power)/row['Speed']) - (f1_car_weight * row['Acceleration'])
 
+            if 'all' not in data[team]:
+                data[team]['all'] = lap
+                drs_data[team]['all'] = drs_lap
+            else:
+                data[team]['all'] = pd.concat([data[team]['all'], lap], ignore_index=True)
+                drs_data[team]['all'] = pd.concat([drs_data[team]['all'], drs_lap], ignore_index=True)
+            
             if race not in data[team]:
                 data[team][race] = lap
                 drs_data[team][race] = drs_lap
@@ -121,12 +145,7 @@ for race in races:
                 data[team][race] = pd.concat([data[team][race], lap], ignore_index=True)
                 drs_data[team][race] = pd.concat([drs_data[team][race], drs_lap], ignore_index=True)
 
-            if 'all' not in data[team]:
-                data[team]['all'] = lap
-                drs_data[team]['all'] = drs_lap
-            else:
-                data[team]['all'] = pd.concat([data[team]['all'], lap], ignore_index=True)
-                drs_data[team]['all'] = pd.concat([drs_data[team]['all'], drs_lap], ignore_index=True)
+            
 
 output_directory = "team_data"
 if not os.path.exists(output_directory):
@@ -147,14 +166,25 @@ for team in drs_data:
     drs_data[team]['all'].to_csv(os.path.join(output_folder, "drs_data.csv"), index=False)
 
 
+all_data = {
+    'Alfa Romeo': {},
+    'AlphaTauri': {},
+    'Alpine': {},
+    'Aston Martin': {},
+    'Ferrari': {},
+    'Haas': {},
+    'McLaren': {},
+    'Mercedes': {},
+    'Red Bull': {},
+    'Williams': {}
+}
+
 
 for team in data:
     #plt.scatter(data[team]['all']['v^2 (m^2/s^4)'], data[team]['all']['F_lost (N)'], label=team)
     regressions_data = []
     for race in data[team]:
         # Perform linear regression
-    
-        
         try:
             coeffs = np.polyfit(data[team][race]['v^2 (m^2/s^4)'], data[team][race]['F_lost (N)'], 1)
         except:
@@ -171,6 +201,10 @@ for team in data:
         # Calculate coefficients
         c_d_mul_a = coeffs[0] / avg_air_density_div_2
         c_rr = coeffs[1] / (normal_force * 4)
+
+        if race == 'all':
+            all_data[team]['c_d_mul_a'] = c_d_mul_a
+            all_data[team]['c_rr'] = c_rr
 
 
         # Append data to list
@@ -202,6 +236,10 @@ for team in drs_data:
         drs_c_d_mul_a = drs_coeffs[0] / avg_air_density_div_2
         drs_c_rr = drs_coeffs[1] / (normal_force * 4)
 
+        if race == 'all':
+            all_data[team]['drs_c_d_mul_a'] = drs_c_d_mul_a
+            all_data[team]['drs_c_rr'] = drs_c_rr
+
         # Append data to list
         drs_regressions_data.append({'Race': race, 'Regression_Line': drs_regression_line, 'C_d_mul_A': drs_c_d_mul_a, 'C_rr': drs_c_rr})
         drs_regressions_df = pd.DataFrame(drs_regressions_data)
@@ -214,31 +252,29 @@ for team in drs_data:
 # plt.legend()
 # plt.show()
 
-# for team in drs_data:
-#     plt.scatter(drs_data[team]['all']['v^2 (m^2/s^4)'], drs_data[team]['all']['F_lost (N)'], label=team)
+for team in drs_data:
+    plt.scatter(drs_data[team]['Italian Grand Prix']['v^2 (m^2/s^4)'], drs_data[team]['Italian Grand Prix']['F_lost (N)'], label=team_to_car[team])
 
-#     # Perform linear regression
-#     coeffs = np.polyfit(drs_data[team]['all']['v^2 (m^2/s^4)'], drs_data[team]['all']['F_lost (N)'], 1)
-#     regression_line = np.poly1d(coeffs)
 
-#     # Plot regression line
-#     plt.plot(drs_data[team]['all']['v^2 (m^2/s^4)'], regression_line(drs_data[team]['all']['v^2 (m^2/s^4)']), label=f'Regression Line ({team})')
+plt.xlabel('v^2 (m^2/s^4)')
+plt.ylabel('F_lost (N)')
+plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Car with DRS in the Italian Grand Prix', fontsize=10)
+plt.legend()
+plt.savefig('ITALY-ALL-DRS.png', bbox_inches='tight')
 
-#     # Display equation of regression line
-#     plt.text(0.1, 0.9, f"y = {coeffs[0]:.2f}x + {coeffs[1]:.2f}", transform=ax.transAxes)
-    
-#     # Calculate coefficients
-#     c_d_mul_a = coeffs[0] / avg_air_density_div_2
-#     c_rr = coeffs[1] / (normal_force * 4)
+plt.show()
 
-#     # Append data to list
-#     drs_regressions_data.append({'Team': team, 'Regression_Line': regression_line, 'C_d_mul_A': c_d_mul_a, 'C_rr': c_rr})
+for team in data:
+    plt.scatter(data[team]['Italian Grand Prix']['v^2 (m^2/s^4)'], data[team]['Italian Grand Prix']['F_lost (N)'], label=team_to_car[team])
 
-# plt.xlabel('v^2 (m^2/s^4)')
-# plt.ylabel('F_lost (N)')
-# plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Team with Regression Lines (DRS)')
-# plt.legend()
-# plt.show()
+
+plt.xlabel('v^2 (m^2/s^4)')
+plt.ylabel('F_lost (N)')
+plt.title('F_lost (N) vs v^2 (m^2/s^4) for Each Car without DRS in the Italian Grand Prix', fontsize=10)
+plt.legend()
+plt.savefig('ITALY-ALL-NO-DRS.png', bbox_inches='tight')
+plt.show()
+
 
 """
 for team, lap_data in data.items():
@@ -261,19 +297,40 @@ plt.legend()
 plt.show() """
 
 
-plt.scatter(data['Red Bull']['Las Vegas Grand Prix']['v^2 (m^2/s^4)'], data['Red Bull']['Las Vegas Grand Prix']['F_lost (N)'], label='RB19')
+plt.scatter(data['Red Bull']['Italian Grand Prix']['v^2 (m^2/s^4)'], data['Red Bull']['Italian Grand Prix']['F_lost (N)'], label='RB19')
 plt.xlabel('v^2 (m^2/s^4)')
 plt.ylabel('F_lost (N)')
-plt.title('F_lost (N) vs v^2 (m^2/s^4) for the RB19 without DRS')
+plt.title('F_lost (N) vs v^2 (m^2/s^4) for the RB19 without DRS in the Italian Grand Prix', fontsize=10)
 plt.legend()
+plt.savefig('ITALY-RB19-NO-DRS.png', bbox_inches='tight')
 plt.show()
 
-plt.savefig('LV-RB19-NO-DRS.png', bbox_inches='tight')
-
-plt.scatter(drs_data['Red Bull']['Las Vegas Grand Prix']['v^2 (m^2/s^4)'], drs_data['Red Bull']['Las Vegas Grand Prix']['F_lost (N)'], label='RB19')
+plt.scatter(drs_data['Red Bull']['Italian Grand Prix']['v^2 (m^2/s^4)'], drs_data['Red Bull']['Italian Grand Prix']['F_lost (N)'], label='RB19')
 plt.xlabel('v^2 (m^2/s^4)')
 plt.ylabel('F_lost (N)')
-plt.title('F_lost (N) vs v^2 (m^2/s^4) for the RB19 with DRS')
+plt.title('F_lost (N) vs v^2 (m^2/s^4) for the RB19 with DRS in the Italian Grand Prix', fontsize=10)
 plt.legend()
+plt.savefig('ITALY-RB19-DRS.png', bbox_inches='tight')
 plt.show()
-plt.savefig('LV-RB19-DRS.png', bbox_inches='tight')
+
+all_drs_data = {
+    'Alfa Romeo': {},
+    'AlphaTauri': {},
+    'Alpine': {},
+    'Aston Martin': {},
+    'Ferrari': {},
+    'Haas': {},
+    'McLaren': {},
+    'Mercedes': {},
+    'Red Bull': {},
+    'Williams': {}
+}
+
+for team in all_data:
+    all_data[team]['cd_mul_a_diff'] = all_data[team]['c_d_mul_a'] - all_data[team]['drs_c_d_mul_a']
+    all_data[team]['c_rr_diff'] = all_data[team]['c_rr'] - all_data[team]['drs_c_rr']
+    all_data[team]['car'] = team_to_car[team]
+    all_data_df = pd.DataFrame(all_data[team])
+    all_data_df.to_csv('all_data.csv', index=False)
+
+
